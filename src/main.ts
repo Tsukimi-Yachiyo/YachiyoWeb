d9d5010d30f17a4b255ae6ee9f7cca29fa18d1e9
Yuki <yifan@liii.pro>
2026-04-02 22:25:33 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Message from './components/Message.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).component('Message', Message).mount('#app')
+createApp(App).use(router).component('Message', Message).use(ElementPlus).mount('#app')
